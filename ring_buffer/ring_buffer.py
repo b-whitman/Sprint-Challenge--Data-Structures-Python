42f45b276accc082e1327e01068690f2a88b501b
Benjamin Whitman <benjamin.whitman90@gmail.com>
2020-09-25 21:24:04 -0400

    filled in __init__()

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,6 +1,10 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        # Create a list to hold values
+        self.buffer = [None for i in range(capacity)]
+        # Create a cursor to track insert position
+        self.cursor = 0
 
     def append(self, item):
         pass
